853ff03a3e9a0c95d34b9c1d8f713ed7281d37c0
Ivan Saorin <ivan.saorin@gmail.com>
2024-10-20 20:19:00 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,5 @@
-import logging
-import colorlog
+def main():
+print("Hello, World!")
 
-# Create a logger object
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# Create a colored console handler
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = colorlog.ColoredFormatter(
-    '%(log_color)s%(levelname)s:%(message)s%(reset)s'
-)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
-# Log some messages with different levels
-logger.debug('This is a debug message')
-logger.info('This is an info message')
-logger.warning('This is a warning message')
-logger.error('This is an error message')
-logger.critical('This is a critical message')
+if name == "main":
+main()
